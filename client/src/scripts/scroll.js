1217295c3f39ce3d316fc2d5888842213ac4c172
Sean Weber <sean.weber95@gmail.com>
2021-07-17 00:33:44 -0400

    added annotation to script.js and webpack.config.js

--- client/src/scripts/scroll.js
+++ client/src/scripts/scroll.js
@@ -1,7 +1,9 @@
 export default function scroll() {
 
+    //sets element to be manipulated as #backToTop
     var myID = document.getElementById("backToTop");
 
+    //shows button if not at top of page
     var myScrollFunc = function () {
         var y = window.scrollY;
         if (y >= 100) {
@@ -11,5 +13,6 @@ export default function scroll() {
         }
     };
 
+    //listens for scroll event to trigger function
     window.addEventListener("scroll", myScrollFunc);
 };
