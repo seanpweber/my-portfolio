24609c08d47dcf9c6285853977d1ff2d6bcc9314
Sean Weber <sean.weber95@gmail.com>
2021-07-17 03:00:12 -0400

    updated background photo in webpack bundle

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ require("./style/style.css");
 //imports
 import scroll from './scripts/scroll.js';
 import profilePhotoSrc from './images/profilePhoto.jpg';
+import computerSrc from './images/computer.jpg';
 import calculatorSrc from './images/calculator.jpg';
 import passwordSrc from './images/password.jpg';
 import plannerSrc from './images/planner.jpg';
@@ -11,6 +12,7 @@ import weatherSrc from './images/weather.jpg';
 
 //image destinations
 var profilePhotoImg = document.getElementById('profilePhoto');
+var computerImg = document.getElementById('computer');
 var calculatorImg = document.getElementById('calculator');
 var passwordImg = document.getElementById('password');
 var plannerImg = document.getElementById('planner');
@@ -18,6 +20,7 @@ var weatherImg = document.getElementById('weather');
 
 //image sources
 profilePhotoImg.src = profilePhotoSrc;
+computerImg.src = computerSrc;
 calculatorImg.src = calculatorSrc;
 passwordImg.src = passwordSrc;
 plannerImg.src = plannerSrc;
