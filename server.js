732c20bcb87db4035f58793c89e8421eb0dc5ba5
Sean Weber <sean.weber95@gmail.com>
2021-07-16 22:09:12 -0400

    same thing

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 console.log("server started");
 
-const express = require('express');
-const app = express();
-const serv = require('http').Server(app);
-const port = process.env.PORT || 8080;
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+var port = process.env.PORT || 8080;
 
 app.get('/',function(req, res) {//listens for when the client opens the site.
     res.sendFile(__dirname + '/client/public/index.html');
